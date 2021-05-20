d09c4167d757d98d1c659f9faa461c18e1b3130d
Mauro Mariuzzo <mauro.mariuzzo@smc.it>
2021-05-20 13:37:08 +0200

    iImplement buffer logig with 5 seconds delay

--- modules/apps/portal/portal-fragment-bundle-watcher/src/main/java/com/liferay/portal/fragment/bundle/watcher/internal/PortalFragmentBundleWatcher.java
+++ modules/apps/portal/portal-fragment-bundle-watcher/src/main/java/com/liferay/portal/fragment/bundle/watcher/internal/PortalFragmentBundleWatcher.java
@@ -16,13 +16,23 @@ package com.liferay.portal.fragment.bundle.watcher.internal;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
@@ -65,15 +75,14 @@ public class PortalFragmentBundleWatcher {
 
 		Bundle systemBundle = _bundleContext.getBundle(0);
 
-		FrameworkWiring frameworkWiring = systemBundle.adapt(
-			FrameworkWiring.class);
+		_frameworkWiring = systemBundle.adapt(FrameworkWiring.class);
 
 		_resolvedBundleListener = bundleEvent -> {
 			Bundle bundleEventBundle = bundleEvent.getBundle();
 
 			if (((bundleEvent.getType() == BundleEvent.INSTALLED) &&
-				 (bundleEventBundle.getState() != Bundle.UNINSTALLED) &&
-				 _isFragment(bundleEventBundle)) ||
+				(bundleEventBundle.getState() != Bundle.UNINSTALLED) &&
+				_isFragment(bundleEventBundle)) ||
 				(bundleEvent.getType() == BundleEvent.RESOLVED)) {
 
 				Map<Bundle, String> installedFragmentBundles =
@@ -130,8 +139,28 @@ public class PortalFragmentBundleWatcher {
 					}
 				}
 
+				if (_log.isDebugEnabled()) {
+					StringBuilder sb = new StringBuilder();
+
+					sb.append(bundleEventBundle.toString());
+
+					if (!hostBundles.isEmpty()) {
+						sb.append(" refresh ");
+
+						for (Bundle bundle : hostBundles) {
+							sb.append(bundle.getSymbolicName()).append(" ");
+						}
+					}
+					else {
+						sb.append(" no refresh ");
+					}
+
+					_log.debug(sb.toString());
+				}
+
 				if (!hostBundles.isEmpty()) {
-					frameworkWiring.refreshBundles(hostBundles);
+					//frameworkWiring.refreshBundles(hostBundles);
+					_updateBuffer(hostBundles);
 				}
 			}
 		};
@@ -144,6 +173,14 @@ public class PortalFragmentBundleWatcher {
 		_bundleContext.removeBundleListener(_resolvedBundleListener);
 
 		_installedFragmentBundleTracker.close();
+
+		if (_timer != null) {
+			_timer.cancel();
+
+			_timer = null;
+		}
+
+		_refreshBundles(true);
 	}
 
 	private String _getFragmentHost(Bundle bundle) {
@@ -178,12 +215,89 @@ public class PortalFragmentBundleWatcher {
 		return false;
 	}
 
+	private void _refreshBundles(boolean force) {
+		long now = System.currentTimeMillis();
+
+		System.out.println("fire " + now);
+
+		if (_hostBuffer.isEmpty() && _timer != null) {
+			_timer.cancel();
+
+			_timer = null;
+		}
+
+		Iterator<Long> iterator = _hostBuffer.keySet().iterator();
+
+		while (iterator.hasNext()) {
+			long time = iterator.next();
+
+			if (force || (now - time > (_DELAY * 2))) {
+				Set<Bundle> bundles = _hostBuffer.remove(time);
+
+				System.out.println("fire " + time + " " + StringUtil.merge(bundles));
+
+				if (bundles != null && !bundles.isEmpty()) {
+					_frameworkWiring.refreshBundles(bundles);
+				}
+			}
+		}
+	}
+
+	private void _updateBuffer(List<Bundle> hostBundles) {
+		long now = System.currentTimeMillis();
+
+		System.out.println("updateBuffer " + now + " " + StringUtil.merge(hostBundles));
+
+		Set<Bundle> buffer = null;
+
+		for (long time : _hostBuffer.keySet()) {
+			if (now - time < _DELAY) {
+				buffer = _hostBuffer.get(time);
+
+				System.out.println("reuse " + time);
+
+				break;
+			}
+		}
+
+		if (buffer == null) {
+			System.out.println("create " + now);
+
+			buffer = new HashSet<Bundle>();
+
+			_hostBuffer.put(now, buffer);
+
+			TimerTask timerTask = new TimerTask() {
+
+				@Override
+				public void run() {
+					_refreshBundles(false);
+				}
+
+			};
+
+			_timer = new Timer(true);
+
+			_timer.schedule(timerTask, _DELAY, _DELAY);
+		}
+
+		buffer.addAll(hostBundles);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalFragmentBundleWatcher.class);
+
+	private static final long _DELAY = 5000L;
+
 	private BundleContext _bundleContext;
+	private FrameworkWiring _frameworkWiring;
+	private Map<Long, Set<Bundle>> _hostBuffer = new ConcurrentHashMap<>();
 	private BundleTracker<String> _installedFragmentBundleTracker;
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
 	private ModuleServiceLifecycle _moduleServiceLifecycle;
 
 	private BundleListener _resolvedBundleListener;
+	private Timer _timer;
 
 }
